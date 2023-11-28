974dc2fb294164ebbc3c6a0ed9a284798a13cae3
Adarsh <89762247+ad-arsh@users.noreply.github.com>
2023-11-28 19:36:11 +0530

    Update NavPopulater.js

--- js/NavPopulater.js
+++ js/NavPopulater.js
@@ -2,7 +2,7 @@ const header = document.getElementById("header");
 
 const nav = `<nav>
 <div class="logo">
-    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGv4rPuhkhg40pqv2lgpAN7UOQGEelKxHZ85DJGh-D6OIrvFG4fa64uM8gPUoXtMvwPI6dsYVEnR55E74NxOTo-Iw3CTBMuk0oeayMvsQLBEXLnk0dp0E1_CVFCIN4uL0CChqXW10monHwehyphenhyphenQNzgFIkuSiDggPxf1c-2TxfryCnD8WWrMWbMDcg20_72e/s832/logo%20eclock.png" alt="" style="height: 70px;">
+    <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGv4rPuhkhg40pqv2lgpAN7UOQGEelKxHZ85DJGh-D6OIrvFG4fa64uM8gPUoXtMvwPI6dsYVEnR55E74NxOTo-Iw3CTBMuk0oeayMvsQLBEXLnk0dp0E1_CVFCIN4uL0CChqXW10monHwehyphenhyphenQNzgFIkuSiDggPxf1c-2TxfryCnD8WWrMWbMDcg20_72e/s832/logo%20eclock.png" alt="" style="height: 60px;">
 </div>
 <ul class="links">
 
