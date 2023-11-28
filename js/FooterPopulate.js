5f9a6600f0f0c71e2ed468ecd7301f80f2044f92
Adarsh <89762247+ad-arsh@users.noreply.github.com>
2023-11-28 19:46:45 +0530

    Update FooterPopulate.js

--- js/FooterPopulate.js
+++ js/FooterPopulate.js
@@ -2,7 +2,7 @@ const footer = document.getElementById("footer");
 
 const ftr = `
 <div class="about">
-            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGv4rPuhkhg40pqv2lgpAN7UOQGEelKxHZ85DJGh-D6OIrvFG4fa64uM8gPUoXtMvwPI6dsYVEnR55E74NxOTo-Iw3CTBMuk0oeayMvsQLBEXLnk0dp0E1_CVFCIN4uL0CChqXW10monHwehyphenhyphenQNzgFIkuSiDggPxf1c-2TxfryCnD8WWrMWbMDcg20_72e/s832/logo%20eclock.png" alt="Rigga" style="height:60px;">
+            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjGv4rPuhkhg40pqv2lgpAN7UOQGEelKxHZ85DJGh-D6OIrvFG4fa64uM8gPUoXtMvwPI6dsYVEnR55E74NxOTo-Iw3CTBMuk0oeayMvsQLBEXLnk0dp0E1_CVFCIN4uL0CChqXW10monHwehyphenhyphenQNzgFIkuSiDggPxf1c-2TxfryCnD8WWrMWbMDcg20_72e/s832/logo%20eclock.png" alt="Rigga" style="height:80px;">
             <p>EClock Industries is one of the leading names in the country in the Electrical domain, be it LED Lighting,
                 Modular Switches & Accessories, Cables, Wires Distribution Switchgear. Conduct Pipes & Accessories with
                 a nation-wide presence and network
